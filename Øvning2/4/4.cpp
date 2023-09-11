287f35f7051d0b59bbec959c48d4ebb6b2789323
flippi321 <90700334+flippi321@users.noreply.github.com>
2023-09-11 23:09:54 +0200

    Added "&" in front of b as it is a reference and not an integer

--- Øvning2/4/4.cpp
+++ Øvning2/4/4.cpp
@@ -12,10 +12,10 @@ int main(int argc, char *argv[])
 {
     // Fixed
     int a = 5;
-    int b = a;   // Originally &b
-    int *c = &b; // While declaring a empty pointer is fine, we need to specify what it points at if we want it to work
-    a = b + *c;  // a is not a pointer, so can't be refrenced with *. Neither is b in this case
-    b = 2;       // We can't manually give b a position, we can however change it's value
+    int &b = a;     // Reference needs to point at a value
+    int *c = &b;    // While declaring a empty pointer is fine, we need to specify what it points at if we want it to work
+    a = b + *c;     // a is not a pointer, so can't be refrenced with *. Neither is b in this case
+    b = 2;          // We can't manually give b a position, we can however change it's value
 
     return 0;
 }
