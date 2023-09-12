8a3309fb2233c64699ac1a80d4189489d7eb3e86
flippi321 <90700334+flippi321@users.noreply.github.com>
2023-09-12 13:27:59 +0200

    Found another issue in task 2.3

--- Øvning2/3/3.cpp
+++ Øvning2/3/3.cpp
@@ -7,17 +7,17 @@ int main(int argc, char *argv[])
     char search_for = 'e';
 
     // Issue 1: Buffer Overflow - Input can exceed the buffer size allocated for 'text'.
+    // Issue 2: cin must be intialized using std::cin
     cin >> text;
 
-    // Issue 2: Infinite Loop - In the absence of 'e' in the input, the loop will never terminate.
+    // Issue 3: Infinite Loop - In the absence of 'e' in the input, the loop will never terminate.
     while (*pointer != search_for)
     {
-        // Issue 3: Data Overwriting - Existing characters are overwritten until 'e' is encountered.
+        // Issue 4: Data Overwriting - Existing characters are overwritten until 'e' is encountered.
         *pointer = search_for;
         pointer++;
-
         
-        // Issue 4: Lack of Boundary Check - 'pointer' could exceed the bounds of the 'text' buffer.
+        // Issue 5: Lack of Boundary Check - 'pointer' could exceed the bounds of the 'text' buffer.
     }
 
     return 0;
