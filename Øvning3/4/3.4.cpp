de673945c03948b06f05cf6bb736695232120429
flippi321 <90700334+flippi321@users.noreply.github.com>
2023-09-14 14:44:43 +0200

    Added task3.4  a and b

---
+++ Øvning3/4/3.4.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {    
+    string word1;
+    string word2;
+    string word3;
+
+    // Task A
+    cout << "Hva er ditt første ord?" << endl;
+    cin >> word1;
+    cout << "Hva er ditt andre ord?" << endl;
+    cin >> word2;
+    cout << "Hva er ditt tredje ord?" << endl;
+    cin >> word3;
+    
+    // Task  B
+    string sentence = word1 + " " +  word2 + " " +  word3 + ".";
+
+    
+}
